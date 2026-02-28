b7fea42f45e16d2a2c2c5f533aafdec95912c682
nine-glitch <laura.amarillaa@gmail.com>
2026-02-27 22:27:01 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'carlitos-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(['/']))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
